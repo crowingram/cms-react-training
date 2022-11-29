aec2a3e40f7bf7f17372c08306f7a17850230f59
Matthew Waldrop <webmaster@crowingram.com>
2022-11-29 11:03:32 -0700

    Imported detail, thumbnail, creator data

--- exercise-1/components/Comic.js
+++ exercise-1/components/Comic.js
@@ -5,10 +5,20 @@ import styles from '@/styles/Comic.module.css'
 
 
 export const Comic = ({ comic }) => {
+	let thumbnailUrl = ''
+	
+	if(comic.thumbnail) {
+		thumbnailUrl=comic.thumbnail.path + "." + comic.thumbnail.extension
+	}
+	
 	return (
 		<div className={styles.card}>
 			<div className={styles.thumbnail}>
-				<Image src={comic.thumbnail} width={150} height={231} alt={comic.title} />
+				{comic.thumbnail 
+					&& comic.thumbnail.path !== "image_not_available " 
+					&& console.log(comic.thumbnail.path + "." + comic.thumbnail.extension)}
+				{comic.thumbnail 
+					&& <Image src={thumbnailUrl} width={150} height={231} alt={comic.title} />}
 				<Button key={comic.id} />
 			</div>
 			<h2 key={comic.id}>{comic.title}</h2>
--- exercise-1/components/Detail.js
+++ exercise-1/components/Detail.js
@@ -1,17 +1,20 @@
 import moment from 'moment';
 import Link from 'next/link';
 import styles from '@/styles/Detail.module.css'
+const API_PUBLIC = process.env.PUBLIC_API_KEY
 
 export function Detail({ comic }) {
+	let creatorUrl = ''
 
 	return (
 		<div className={styles.detail}>
 			{ comic.issueNumber > 0 && <p><b>Issue:</b> {comic.issueNumber}</p> }
 			<p><b>Published:</b> { moment(comic.publishDate).format('LL') }</p>
-			<p><b>Creators:</b></p>
-			{ comic.creators.map((creator) => {
+			{ comic.creators && <p><b>Creators:</b></p>}
+			{ comic.creators && comic.creators.items.map((creator) => {
 				let index = Math.random()
-				return <p key={index}><Link href={creator.resourceURI}>{creator.name}</Link>, {creator.role}</p>
+				creatorUrl = creator.resourceURI + "?apikey=" + API_PUBLIC
+				return <p key={index}><Link href={creatorUrl}>{creator.name}</Link>, {creator.role}</p>
 			})}
 			
 		</div>
